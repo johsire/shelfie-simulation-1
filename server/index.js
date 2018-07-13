ac3c7727f293457cfc1bdc7421cc9363294d0e3a
Josiah Maina <josiah5115@gmail.com>
2018-07-13 12:41:51 -0600

    used massive to link-up db to app

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const massive = require('massive');
+require('dotenv').config();
 
 const controller = require('./controller');
 
 const app = express();
+massive(process.env.CONNECTION_STRING)
+  .then(dbInstance => app.set('db', dbInstance));
 
 app.use(bodyParser.json());
+app.use(cors());
 
 
 
